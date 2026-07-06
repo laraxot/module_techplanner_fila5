e1708c8be39c849409126222bc82dea92f728047
marco76tv <marco.sottana@gmail.com>
2026-07-06 21:17:39 +0200

    .

--- tests/Unit/Models/BaseModelTest.php
+++ tests/Unit/Models/BaseModelTest.php
@@ -7,36 +7,32 @@ namespace Modules\TechPlanner\Tests\Unit\Models;
 use Illuminate\Database\Eloquent\Model;
 use Modules\TechPlanner\Models\BaseModel;
 
-beforeEach(function () {
-    /** @var \Modules\TechPlanner\Tests\TestCase $this */
-        $this->baseModel = new class() extends BaseModel
+function createTechPlannerTestBaseModel(): BaseModel
+{
+    return new class() extends BaseModel
     {
         protected $table = 'test_techplanner_table';
     };
-});
+}
 
 test('base model extends eloquent model', function () {
-    /** @var \Modules\TechPlanner\Tests\TestCase $this */
-        expect($this->baseModel)->toBeInstanceOf(Model::class);
+    expect(createTechPlannerTestBaseModel())->toBeInstanceOf(Model::class);
 });
 
 test('base model has correct table name', function () {
-    /** @var \Modules\TechPlanner\Tests\TestCase $this */
-        expect($this->baseModel->getTable())->toBe('test_techplanner_table');
+    expect(createTechPlannerTestBaseModel()->getTable())->toBe('test_techplanner_table');
 });
 
 test('base model can be instantiated', function () {
-    /** @var \Modules\TechPlanner\Tests\TestCase $this */
-        expect($this->baseModel)->toBeInstanceOf(BaseModel::class);
+    expect(createTechPlannerTestBaseModel())->toBeInstanceOf(BaseModel::class);
 });
 
 test('base model has proper inheritance chain', function () {
-    /** @var \Modules\TechPlanner\Tests\TestCase $this */
-        expect($this->baseModel)->toBeInstanceOf(BaseModel::class);
-    expect($this->baseModel)->toBeInstanceOf(Model::class);
+    $baseModel = createTechPlannerTestBaseModel();
+    expect($baseModel)->toBeInstanceOf(BaseModel::class);
+    expect($baseModel)->toBeInstanceOf(Model::class);
 });
 
 test('base model has timestamps enabled', function () {
-    /** @var \Modules\TechPlanner\Tests\TestCase $this */
-        expect($this->baseModel->usesTimestamps())->toBeTrue();
+    expect(createTechPlannerTestBaseModel()->usesTimestamps())->toBeTrue();
 });
